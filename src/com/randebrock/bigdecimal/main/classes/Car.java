b8ddf2c04218aac9d96c4db87fcc391c42c51112
Boris Michael Randebrock <boris@randebrock.com>
2023-01-19 16:29:58 +0100

    added abstract cars & sons with their setters and getters

---
+++ src/com/randebrock/bigdecimal/main/classes/Car.java
@@ -0,0 +1,47 @@
+package com.randebrock.bigdecimal.main.classes;
+
+abstract class Car {
+    private int vinNumber;
+    private String make;
+    private String model;
+    private int mileage;
+
+    public Car(int vinNumber, String make, String model, int mileage) {
+        this.vinNumber = vinNumber;
+        this.make = make;
+        this.model = model;
+        this.mileage = mileage;
+    }
+
+    public int getVinNumber() {
+        return vinNumber;
+    }
+
+    public void setVinNumber(int vinNumber) {
+        this.vinNumber = vinNumber;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+}
